216b0773bbba594443f00510387820d4663adfab
vPryhodko <78150377+vPryhodko@users.noreply.github.com>
2021-01-28 15:12:00 +0300

    Добавлен первый файл

---
+++ MyFirstProgram.java
@@ -0,0 +1,10 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        String somebody = "world";
+        System.out.println("Hello, " + somebody + "!");
+
+        double l = 8;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + 1 + " = " + s);
+    }
+}
